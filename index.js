9ca52c0c2e7d064af8daaa88e941fce03e7ef66b
Alex Kondratyuk <lynxtaa@gmail.com>
2017-03-24 22:06:54 +0300

    Added check for Proxy support

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const promisify = (fn, context) => (...args) => new Promise((resolve, reject) => {
 	args.push((err, data) => err ? reject(err) : resolve(data))
 	fn.apply(context, args)
-});
+})
 
 promisify.all = srcObj => new Proxy(srcObj, {
 	get(target, key, context) {
 		return promisify(target[key], target)
 	}
 })
+
+try {
+	new Proxy({}, {})
+} catch (e) {
+	promisify.all = () => {
+		throw new Error('ES6 Proxy is not supported')
+	}
+}
 
 module.exports = promisify
